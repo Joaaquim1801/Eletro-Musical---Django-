e527ac6e72b0d102670151d4f3caf6f482e23c6b
Joaaquim1801 <joaquimaiafilho2018@gmail.com>
2025-06-23 21:34:46 -0300

    Implement product catalog page and add logout functionality; update styles and add WhatsApp button

--- usuarios/views.py
+++ usuarios/views.py
@@ -6,3 +6,8 @@ def login(request):
 def cadastro(request):
     return render(request, 'account/signup.html')
 
+def logout(request):
+    return render(request, 'account/logout.html')
+
+def test(request):
+    return render(request, 'usuarios/test.html')
